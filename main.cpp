400c70f29d9f68fcde87319747b6d29e06e511b1
Nikita <nikitosbarbos12345@gmail.com>
2023-12-02 15:12:57 +0100

    changed function policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b)
+{
+    return a*b;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!\na*b=" << Policz(6, 90) << std::endl;
     return 0;
 }
