1e795f10b5c024bdd218bc8207af30f7176b2411
Nikita <nikitosbarbos12345@gmail.com>
2023-12-02 15:08:31 +0100

    add funtion policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b)
+{
+    return a+b;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << Policz(4, 5) <<  std::endl;
     return 0;
 }
